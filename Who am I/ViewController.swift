1a95f1d356fa9b2f99cc73dc11409a0592e4a9e0
Ruslan Bityukov <bitukov_ruslan@mail.ru>
2021-10-30 20:57:02 +0300

    Initial Commit

---
+++ Who am I/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Who am I
+//
+//  Created by Руслан Битюков on 30.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
